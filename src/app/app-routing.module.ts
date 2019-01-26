9039de09b25096a2f1caeeaa217c52af1d63409a
EvanZimmerman <ewz23941@gmail.com>
2019-01-25 22:38:18 -0500

    added more routing and subroutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 
 const appRoutes: Routes = [
     {
@@ -16,8 +18,21 @@ const appRoutes: Routes = [
         component: RecipesComponent,
         children: [
             {
-                path: 'recipes/:id',
+                path: '',
+                component: RecipeStartComponent,
+                pathMatch: 'full'
+            },
+            {
+                path: 'new',
+                component: RecipeEditComponent
+            },
+            {
+                path: ':id',
                 component: RecipeDetailComponent
+            },
+            {
+                path: ':id/edit',
+                component: RecipeEditComponent
             }
         ]
     },
