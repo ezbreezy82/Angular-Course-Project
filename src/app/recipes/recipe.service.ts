66d9bc67483c6f611d17c56a9747e84c9b68b183
EvanZimmerman <ewz23941@gmail.com>
2019-01-25 23:01:38 -0500

    code cleanup and added a subject.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -5,11 +5,10 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe('Tasty Schnitzel',
-                   'A super-tasty Schnitzel - just awesome!',
+                   'A super-tasty schnitzel - just awesome!',
                    'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
                    [
                        new Ingredient('Meat', 1),
