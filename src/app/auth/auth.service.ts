dde4174e7280bad7f2748ceb7c533d4749fd432a
EvanZimmerman <ewz23941@gmail.com>
2019-02-07 22:28:46 -0500

    fixed verbiage and firebase dev warning.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 import { Router } from '@angular/router';
 import { Injectable, EventEmitter } from '@angular/core';
 
